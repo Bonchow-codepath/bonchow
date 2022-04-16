5385fa2d0053a4bcaf2824f415ba95200cfde747
Anagh Kanungo <23740251+anaghkanungo7@users.noreply.github.com>
2022-04-16 12:35:49 -0400

    Version 1.0

---
+++ Bonchow/MapViewController.swift
@@ -0,0 +1,66 @@
+//
+//  MapViewController.swift
+//  Bonchow
+//
+//  Created by Anagh Kanungo on 02/04/22.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    //    To fetch current location
+    var locationManagerVar = CLLocationManager()
+//    lazy var mapView = GMSMapView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.settings.myLocationButton = true;
+        
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 13.0)
+//        mapView.camera = camera;
+        
+        // User Location
+        locationManagerVar.delegate = self
+        locationManagerVar.requestWhenInUseAuthorization()
+        locationManagerVar.desiredAccuracy = kCLLocationAccuracyBest
+        locationManagerVar.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if (status == CLAuthorizationStatus.authorizedWhenInUse)
+          {
+            mapView.isMyLocationEnabled = true
+          }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last
+        mapView.camera = GMSCameraPosition.camera(withTarget: newLocation!.coordinate, zoom: 13.0)
+        
+    }
+    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//            let userLocation = locations.last
+//            let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+//
+//            let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
+//                                                              longitude: userLocation!.coordinate.longitude, zoom: 13.0)
+//            mapView.camera = camera;
+//            mapView.isMyLocationEnabled = true
+//            self.view = mapView
+//
+//        locationManagerVar.stopUpdatingLocation()
+//        }
+    
+//    extension ViewController:
+//        GMSMapViewDelegate  {
+//
+//    }
+ 
+}
---
+++ Bonchow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bonchow
+//
+//  Created by Anagh Kanungo on 02/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
